a0faaeb8cf8093e934837dafb4d58e8ac0a14dd1
Isaac Austad <ikeaustad@gmail.com>
2024-09-13 14:02:22 -0600

    Added rotation on X and Y axis

--- GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
+++ GameObjectsAndTransformLab/Assets/Scripts/TransformController.cs
@@ -5,12 +5,13 @@ public class NewBehaviourScript : MonoBehaviour
     private void Update()
     {
         // Move the target GameObject
-        var x = Mathf.PingPong(Time.time, 3);
+        var x = Mathf.PingPong(Time.time, 3); transform.position = new Vector3(x, 0, 0);
         var p = new Vector3(0, x, 0);
         transform.position = p;
         
         // Rotate the target GameObject
-        transform.Rotate(new Vector3(0,30, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(30, 0, 0) * Time.deltaTime);
+        transform.Rotate(new Vector3(0, 30, 0) * Time.deltaTime);
     }
 
 
