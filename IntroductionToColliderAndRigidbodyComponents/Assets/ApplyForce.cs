13cb373481225f315785f0f7645089cf43e2f1ff
Isaac Austad <ikeaustad@gmail.com>
2024-09-15 15:06:48 -0600

    Added Rigidbody lab and completed tasks

---
+++ IntroductionToColliderAndRigidbodyComponents/Assets/ApplyForce.cs
@@ -0,0 +1,17 @@
+using UnityEngine;
+
+public class ApplyForce : MonoBehaviour
+{
+    private Rigidbody _rb;
+
+    private void Start()
+    {
+        _rb = GetComponent<Rigidbody>();
+        _rb.AddForce(Vector3.right * 500);
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Collision detected with " + collision.gameObject.name);
+    }
+}   
